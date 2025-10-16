5e7efb199c8436f583358da2cf97e2425a93f6fe
Nikhil kumar <nikhilkumar14082005@gmail.com>
2025-10-16 23:20:48 +0530

    changess

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 // Enable CORS
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 
 // Body parser
 app.use(express.json());
