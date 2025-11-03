45984ce3013580b030b5f6b61edc75a15c0d356d
Nikhil kumar <nikhilkumar14082005@gmail.com>
2025-11-03 13:12:26 +0530

    changes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ connectDB();
 // ✅ Enable CORS properly
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://tagtells-frontend.vercel.app"],
     credentials: true,
   })
 );
